fb2ad838d3fdb0754ab46dcca2939ec7f55e9400
Ashwath A B <ashwath2767@gmail.com>
2024-07-23 09:16:06 +0530

    Eposode 3

--- App.js
+++ App.js
@@ -12,16 +12,42 @@ import ReactDOM from "react-dom/client"
 // let root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
 
+let Title = () => (
+  <h1 className="h1_class">
+    Component 1
+  </h1>
+)
 
-let parent = React.createElement("div", {id: "parent"}, [
-  React.createElement("div", {id: "child1"}, [
-    React.createElement("h1", {}, "From h1 tag!! By AAB"),
-    React.createElement("h2", {}, "From h2 tag!!"),
-  ]),
-  React.createElement("div", {id: "child1"}, [
-    React.createElement("h1", {}, "From h1 tag!!"),
-    React.createElement("h2", {}, "From h2 tag!!"),
-  ])
-]);
+let HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    Inside container
+  </div>
+)
+
+let content = (
+  <div id="parent">
+    <div id="child1">
+      <h1>From h1 tag JSX</h1>
+      <h2>From h2 tag JSX</h2>
+    </div>
+    <div id="child2">
+      <h1>From h1 tag JSX</h1>
+      <h2>From h2 tag JSX</h2>
+    </div>
+  </div>
+)
+
+// let content = React.createElement("div", {id: "parent"}, [
+//   React.createElement("div", {id: "child1"}, [
+//     React.createElement("h1", {}, "From h1 tag!! By AAB"),
+//     React.createElement("h2", {}, "From h2 tag!!"),
+//   ]),
+//   React.createElement("div", {id: "child1"}, [
+//     React.createElement("h1", {}, "From h1 tag!!"),
+//     React.createElement("h2", {}, "From h2 tag!!"),
+//   ])
+// ]);
 let root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// root.render(content);  # JSX render
+root.render(<HeadingComponent />);
